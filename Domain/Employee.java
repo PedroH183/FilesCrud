c5c5fd12089f18447246839de79863e6ce29b55f
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2024-02-28 10:43:50 -0300

    feat: adding new domains

---
+++ Domain/Employee.java
@@ -0,0 +1,73 @@
+package Domain;
+
+import Service.DataCompatibility;
+
+public class Employee extends DataCompatibility {
+   private String id;
+   private String name;
+   private String address;
+   private String wage;
+   private String birthDate;
+   private final String type = "Employee";
+   
+   public Employee(String id, String name, String address, String wage, String birthDate) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.wage = wage;
+        this.birthDate = birthDate;
+   }
+
+   public Employee() {
+        this.id = null;
+        this.name = null;
+        this.address = null;
+        this.wage = null;
+        this.birthDate = null;
+   }
+
+    @Override
+    public String getRawData() {
+        return new String(this.type + " | " + this.id + " | " + this.name + " | " + this.address + " | " + this.wage + " | " + this.birthDate);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWage() {
+        return wage;
+    }
+
+    public void setWage(String wage) {
+        this.wage = wage;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ Domain/Vehicle.java
@@ -0,0 +1,88 @@
+package Domain;
+
+import Service.DataCompatibility;
+
+public class Vehicle extends DataCompatibility {
+    private String id;
+    private String employeeId;
+    private String description;
+    private String plate;
+    private String brand;
+    private String model;
+    private final String type = "Vehicle";    
+
+    public Vehicle(String id, String employeeId, String description, String plate, String brand, String model) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.description = description;
+        this.plate = plate;
+        this.brand = brand;
+        this.model = model;
+    }
+
+    public Vehicle() {
+        this.id = null;
+        this.employeeId = null;
+        this.description = null;
+        this.plate = null;
+        this.brand = null;
+        this.model = null;
+    }
+
+    @Override
+    public String getRawData() {
+        return new String(this.type + " | " + this.id + " | " + this.employeeId + " | " + this.description + " | " + this.plate + " | " + this.brand + " | " + this.model);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
