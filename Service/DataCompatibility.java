744c6160bf12b2268aa9c850292426b07ec98d91
PedroH183 <pedro_freires18@outlook.com>
2024-03-01 18:57:33 -0300

    chore: removing spaces

--- Service/DataCompatibility.java
+++ Service/DataCompatibility.java
@@ -3,7 +3,7 @@ package Service;
 public abstract class DataCompatibility {
     protected String id;
     protected String type;
-    
+
     public abstract String getRawData();
     public abstract String getId();
     public abstract String getType();
