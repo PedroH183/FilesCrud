7d6e7b7330060b16122bb32cb71d6dc2bb3be14c
PedroH183 <pedro_freires18@outlook.com>
2024-03-02 14:51:25 -0300

    chore: deleting service folder

--- DBManager/DataCompatibility.java
+++ DBManager/DataCompatibility.java
@@ -1,4 +1,7 @@
-package Service;
+package DBManager;
+
+import java.util.UUID;
+
 
 public abstract class DataCompatibility {
     protected String id;
@@ -7,4 +10,8 @@ public abstract class DataCompatibility {
     public abstract String getRawData();
     public abstract String getId();
     public abstract String getType();
+
+    public static String generateRandomId(){
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 10);
+    }
 }
