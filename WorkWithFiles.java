eb5bfcf69677d155ba9c3ef3fece7eb515762cfc
PedroH183 <pedro_freires18@outlook.com>
2024-03-02 14:50:59 -0300

    feat: menu verifications

--- WorkWithFiles.java
+++ WorkWithFiles.java
@@ -2,9 +2,10 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import DBManager.DBManager;
-import Domain.Employee;
 import Domain.Vehicle;
+import Domain.Employee;
+import DBManager.DBManager;
+import DBManager.DataCompatibility;
 
 
 public class WorkWithFiles {
@@ -27,45 +28,56 @@ public class WorkWithFiles {
           String idEmployee;
 
           switch (option) {
-            case "1":
-              // creating employee
+            case "1": // creating employee
+
               String id, name, address, wage, birthDate;
-              id = menu.setScanner("Id: ", s);
+
+              id = DataCompatibility.generateRandomId();
               name = menu.setScanner("Name: ", s);
               address = menu.setScanner("Address: ", s);
               wage = menu.setScanner("Wage: ", s);
               birthDate = menu.setScanner("Birth Date: ", s);
 
               Employee employee = new Employee(id, name, address, wage, birthDate);
+
               cursor.insertData(employee);
               System.out.print("\n");
+
               break;
 
-            case "2":
-              // Read all employees
+            case "2": // Read all employees
+
               cursor.viewAll("Employee");
               System.out.print("\n");
               break;
 
-            case "3":
-              // Search employees
+            case "3": // Search employees
+
               idEmployee = menu.setScanner("Type the employee id to search: ", s);
+
               cursor.viewEmployee(idEmployee);
               System.out.print("\n");
               break;
 
-            case "4":
-              // Edit employee
+            case "4": // Edit employee
+
               idEmployee = menu.setScanner("Type the employee id to edit: ", s);
+
+              if(cursor.getOneEmployee(idEmployee) == null){
+                System.out.println("Employee not found !!");
+                break;
+              }
+
               HashMap<String, String> menuData = menu.menuUpdateEmployee(s);
 
-              cursor.updateData(idEmployee, menuData);
+              cursor.updateVehicle(idEmployee, menuData);
               System.out.print("\n");
               break;
 
-            case "5":
-              // removing employee
-              idEmployee = menu.setScanner("Type the employee id to remove: ", s);;
+            case "5": // removing employee
+
+              idEmployee = menu.setScanner("Type the employee id to remove: ", s);
+
               cursor.deleteData(idEmployee);
               System.out.print("\n");
               break;
@@ -79,10 +91,11 @@ public class WorkWithFiles {
           option = menu.setScanner("\nChoose an option: ", s);
 
           switch (option) {
-            case "1":
-              // creating vehicle
+            case "1": // creating vehicle
+
               String id, employeeId, description, plate, brand, model;
-              id = menu.setScanner("\nId: ", s);
+
+              id = DataCompatibility.generateRandomId();
               employeeId = menu.setScanner("Employee Id: ", s);
               description = menu.setScanner("Description: ", s);
               plate = menu.setScanner("Plate: ", s);
@@ -94,31 +107,50 @@ public class WorkWithFiles {
               System.out.print("\n");
               break;
 
-            case "2":
-              // listing all vehicle
+            case "2": // listing all vehicle
+
               cursor.viewAll("Vehicle");
               break;
 
-            case "3":
-              // view vehicle
+            case "3": // view vehicle
+
               idEmployee = menu.setScanner("Type the vehicle id to search: ", s);
+
+              if(cursor.getOneVehicle(idEmployee) == null){
+                System.out.println("Vehicle not found !!");
+                break;
+              }
+
               cursor.viewVehicle(idEmployee);
               System.out.print("\n");
               break;
 
-            case "4":
-              // Edit vehicle
+            case "4": // Edit vehicle
+
               idEmployee = menu.setScanner("Type the vehicle id to edit: ", s);
+
+              if(cursor.getOneVehicle(idEmployee) == null){
+                System.out.println("Vehicle not found !!");
+                break;
+              }
+
               HashMap<String, String> menuData = menu.menuUpdateVehicle(s);
 
-              cursor.updateData(idEmployee, menuData);
+              cursor.updateVehicle(idEmployee, menuData);
               break;
 
-            case "5":
-              // removing vehicle
-              idEmployee = menu.setScanner("Type the vehicle id to remove: ", s);;
+            case "5": // removing vehicle
+
+              idEmployee = menu.setScanner("Type the vehicle id to remove: ", s);
+
+              if(cursor.getOneVehicle(idEmployee) == null){
+                System.out.println("Vehicle not found !!");
+                break;
+              }
+
               cursor.deleteData(idEmployee);
               break;
+
             case "6":
               break;
           }
